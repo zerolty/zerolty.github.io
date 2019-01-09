f72b4ec4a4f4be207028701d0f2d7367fcc3f708
徐人杰 <xurenjie@bytedance.com>
2019-01-09 23:58:48 +0800

    add color-design-helper

---
+++ color-design-helper/precache-manifest.cbfa92a214eaf68efddf63e597c8f8a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e0cc5a4e6362ca32aaf",
+    "url": "/static/js/main.0e0cc5a4.chunk.js"
+  },
+  {
+    "revision": "66cd7764b11e3cae4b30",
+    "url": "/static/js/1.66cd7764.chunk.js"
+  },
+  {
+    "revision": "0e0cc5a4e6362ca32aaf",
+    "url": "/static/css/main.547a0b16.chunk.css"
+  },
+  {
+    "revision": "156a853dad58cd319f5664f4f64dc452",
+    "url": "/index.html"
+  }
+];
