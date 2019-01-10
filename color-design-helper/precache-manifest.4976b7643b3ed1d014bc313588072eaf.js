da658cfbdadc0e69ec2d3f6eed8e86d65ac8e3fa
徐人杰 <xurenjie@bytedance.com>
2019-01-10 14:40:27 +0800

    fix color #000

---
+++ color-design-helper/precache-manifest.4976b7643b3ed1d014bc313588072eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e1c7100d60a1bd17e7f3",
+    "url": "./static/js/main.e1c7100d.chunk.js"
+  },
+  {
+    "revision": "66cd7764b11e3cae4b30",
+    "url": "./static/js/1.66cd7764.chunk.js"
+  },
+  {
+    "revision": "e1c7100d60a1bd17e7f3",
+    "url": "./static/css/main.547a0b16.chunk.css"
+  },
+  {
+    "revision": "746f6c880c194a1439a1d509aee4f087",
+    "url": "./index.html"
+  }
+];
